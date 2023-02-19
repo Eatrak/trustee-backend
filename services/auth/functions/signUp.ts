21dfd1b7126f369e87a553eba4843e649ea7dc5c
Davide Vacirca <davidevacirca8@gmail.com>
2023-02-19 09:05:31 +0100

    refactor/DV_UseTurboRepoToStructureTheProject

--- services/auth/functions/signUp.ts
+++ services/auth/functions/signUp.ts
@@ -7,8 +7,8 @@ import { ulid } from "ulid";
 
 import { signUpEnvironmentValidator, signUpValidator } from "@crudValidators/auth";
 import { SignUpBody } from "@bodies/auth/signUp";
-import { Utils } from "@utils/Utils";
-import UsersUtils from "@utils/UsersUtils";
+import { Utils } from "utils/Utils";
+import UsersUtils from "utils/UsersUtils";
 
 // Environment variables
 const USER_POOL_ID = process.env.USER_POOL_ID!;
--- src/shared/crudValidators/transactions.ts
+++
@@ -1,4 +0,0 @@
-export const getTransactionsValidator = {
-    startCreationTimestamp: "integer",
-    endCreationTimestamp: "integer"
-};
