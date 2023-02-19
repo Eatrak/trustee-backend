f04a3c66559e17c08dfd994a452213e36ec18434
Davide Vacirca <davidevacirca8@gmail.com>
2023-02-19 09:26:41 +0100

    refactor: use turbopack to change the project structure

--- services/auth/functions/signIn.ts
+++ services/auth/functions/signIn.ts
@@ -5,8 +5,8 @@ import Validator from "validatorjs";
 import en from 'validatorjs/src/lang/en';
 
 import { signInEnvironmentValidator, signInValidator } from "@crudValidators/auth";
-import { Utils } from "@utils/Utils";
-import UsersUtils from "@utils/UsersUtils";
+import { Utils } from "utils/Utils";
+import UsersUtils from "utils/UsersUtils";
 import { SignInBody } from "@bodies/auth/signIn";
 
 // Environment variables
--- src/shared/requestInterfaces/transactions/getTransactions.ts
+++
@@ -1,5 +0,0 @@
-import { ITransaction } from "../../entities/transaction";
-
-export interface GetTransactionsResponse {
-    transactions: ITransaction[]
-}
