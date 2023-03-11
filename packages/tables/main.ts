1a3be0c87ad8fa9e4c4ca4703692a96ba995e5ca
Davide Vacirca <davidevacirca8@gmail.com>
2023-03-11 15:08:22 +0100

    update(tables): add main dynamodb table connection with typedorm

---
+++ packages/tables/main.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+import { INDEX_TYPE, Table } from '@typedorm/common';
+
+export const mainTable = new Table({
+  name: `trustee-${process.env.STAGE}`,
+  partitionKey: 'PK',
+  sortKey: 'SK',
+  indexes: {
+    GSI1: {
+      partitionKey: 'GSI1PK',
+      sortKey: 'GSI1SK',
+      type: INDEX_TYPE.GSI,
+    },
+  },
+});
