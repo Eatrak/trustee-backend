c6ccee44f2a793cacfa7af31785ca0dff83ad35f
Davide Vacirca <davidevacirca8@gmail.com>
2023-05-27 12:23:44 +0200

    feat: add api used to check if user is authenticated

--- libs/types/auth.ts
+++ libs/types/auth.ts
@@ -1,3 +1,12 @@
 export interface AuthorizerCustomClaims {
     userId: string
 }
+
+export interface JsonWebKey {
+    alg: string,
+    e: string,
+    kid: string,
+    kty: string,
+    n: string,
+    use: string
+}
