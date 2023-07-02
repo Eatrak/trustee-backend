c209120260306e5d2a7c660178381e53c9e57384
Davide Vacirca <davidevacirca8@gmail.com>
2023-07-02 17:50:56 +0200

    feat(statistics): add util used to get a monthly-wallet-income item

--- packages/utils/MonthlyWalletIncomeUtils.ts
+++ packages/utils/MonthlyWalletIncomeUtils.ts
@@ -1,7 +1,10 @@
 import "reflect-metadata";
+import { Ok, Err, Result } from "ts-results";
 
 import { MonthlyWalletIncome } from "entities/monthlyWalletIncome";
 import DatabaseUtils from "./DatabaseUtils";
+
+export type Errors = "UNEXISTING_RESOURCE" | "GENERAL";
 
 export default class MonthlyWalletIncomeUtils {
     /**
@@ -20,5 +23,40 @@ export default class MonthlyWalletIncomeUtils {
             { queryIndex: "GSI1" }
         );
         return response;
+    }
+
+    /**
+     * Get monthly-wallet-income.
+     * 
+     * @param userId ID of the user.
+     * @param walletId ID of a user wallet.
+     * @param year Year of the wallet income.
+     * @param month Month of the wallet income.
+     * @returns Monthly-wallet-income.
+     */
+    public static async getMonthlyWalletIncome(
+        userId: string,
+        walletId: string,
+        year: number,
+        month: number
+    ): Promise<Result<MonthlyWalletIncome, Errors>> {
+        try {
+            // Get monthly-wallet-income
+            const monthlyWalletIncome = await DatabaseUtils.getInstance().getEntityManager().findOne(
+                MonthlyWalletIncome,
+                { userId, walletId, year, month }
+            );
+
+            // Abort if the monthly-wallet-income doesn't exist
+            if (!monthlyWalletIncome) {
+                return Err("UNEXISTING_RESOURCE");
+            }
+
+            return Ok(monthlyWalletIncome);
+        }
+        catch (err) {
+            console.log(err);
+            return Err("GENERAL");
+        }
     }
 }
