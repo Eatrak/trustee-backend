3a28070767cb7a60295af8ac49a2fc8faf13dd3b
Davide Vacirca <davidevacirca8@gmail.com>
2023-07-02 17:50:32 +0200

    feat(statistics): add util used to get a monthly-wallet-expense item

--- packages/utils/MonthlyWalletExpenseUtils.ts
+++ packages/utils/MonthlyWalletExpenseUtils.ts
@@ -1,7 +1,10 @@
 import "reflect-metadata";
+import { Ok, Err, Result } from "ts-results";
 
 import { MonthlyWalletExpense } from "entities/monthlyWalletExpense";
 import DatabaseUtils from "./DatabaseUtils";
+
+export type Errors = "UNEXISTING_RESOURCE" | "GENERAL";
 
 export default class MonthlyWalletExpenseUtils {
     /**
@@ -20,5 +23,40 @@ export default class MonthlyWalletExpenseUtils {
             { queryIndex: "GSI1" }
         );
         return response;
+    }
+
+    /**
+     * Get monthly-wallet-expense.
+     * 
+     * @param userId ID of the user.
+     * @param walletId ID of a user wallet.
+     * @param year Year of the wallet expense.
+     * @param month Month of the wallet expense.
+     * @returns Monthly-wallet-expense.
+     */
+    public static async getMonthlyWalletExpense(
+        userId: string,
+        walletId: string,
+        year: number,
+        month: number
+    ): Promise<Result<MonthlyWalletExpense, Errors>> {
+        try {
+            // Get monthly-wallet-expense
+            const monthlyWalletExpense = await DatabaseUtils.getInstance().getEntityManager().findOne(
+                MonthlyWalletExpense,
+                { userId, walletId, year, month }
+            );
+
+            // Abort if the monthly-wallet-expense doesn't exist
+            if (!monthlyWalletExpense) {
+                return Err("UNEXISTING_RESOURCE");
+            }
+
+            return Ok(monthlyWalletExpense);
+        }
+        catch (err) {
+            console.log(err);
+            return Err("GENERAL");
+        }
     }
 }
