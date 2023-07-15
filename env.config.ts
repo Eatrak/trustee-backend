3a96df93bc7b1c5ffa209a5ed574f28ebd35870d
Davide Vacirca <davidevacirca8@gmail.com>
2023-07-15 19:01:40 +0200

    chore(env): add environment configuration file

---
+++ env.config.ts
@@ -0,0 +1,34 @@
+import environments from "env";
+
+type EnvironmentName = "dev" | "prod";
+
+interface EnvironmentVariables {
+    DB_NAME: string,
+    DB_USERNAME: string,
+    DB_HOST: string,
+    DB_PASSWORD: string,
+    DB_PORT: number
+}
+
+export type Environments = {
+    [environmentName in EnvironmentName]?: EnvironmentVariables;
+};
+
+export const currentEnvironmentName: EnvironmentName = "dev";
+
+export const env = environments[currentEnvironmentName]!;
+
+if (!env)
+    throw new Error(`The environment with name "${currentEnvironmentName}" doesn't exist.`);
+
+export const getDbUrl = () => {
+    const {
+        DB_USERNAME,
+        DB_PASSWORD,
+        DB_HOST,
+        DB_PORT,
+        DB_NAME
+    } = env;
+
+    return `mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+};
