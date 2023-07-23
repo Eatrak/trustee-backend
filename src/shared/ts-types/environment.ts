ccff40781f0a806e9ad63024ab7591560414a7d9
Davide Vacirca <davidevacirca8@gmail.com>
2023-07-23 12:20:54 +0200

    refactor(types): add ts environment-configuration interface

---
+++ src/shared/ts-types/environment.ts
@@ -0,0 +1,8 @@
+export interface EnvironmentConfiguration {
+    DB_NAME: string;
+    DB_USERNAME: string;
+    DB_HOST: string;
+    DB_PASSWORD: string;
+    DB_PORT: number;
+    DB_URL: string;
+}
